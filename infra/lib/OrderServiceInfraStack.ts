ed8ac8bae72ceb03fe7a99f3887ba8e48e1819f4
sarthakmakhija <sarthak.makhija@gmail.com>
2020-03-13 22:58:23 +0530

    Sarthak | Adds unit test for lambda function in OrderServiceInfraStack

--- infra/lib/OrderServiceInfraStack.ts
+++ infra/lib/OrderServiceInfraStack.ts
@@ -1,16 +1,16 @@
-import * as cdk from "@aws-cdk/core";
 import {Code, Function, FunctionProps, Runtime} from "@aws-cdk/aws-lambda";
+import {Construct, Stack, StackProps} from "@aws-cdk/core";
 
-export class OrderServiceInfraStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class OrderServiceInfraStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
         const functionProperties: FunctionProps = {
             code: Code.fromAsset("../src"),
             handler: "handler.ordersHandler",
             runtime: Runtime.NODEJS_10_X,
-            functionName: "order-service-lambda"
+            functionName: "order-service-function"
         };
-        new Function(this, "order-service-lambda", functionProperties)
+        new Function(this, "order-service-function", functionProperties)
     }
 }
